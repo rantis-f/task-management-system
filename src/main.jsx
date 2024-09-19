ff80d57ef748fd44bcbaf3f6be96c11ec4bf13e0
rantis-f <fayyadrantisismktibazma@gmail.com>
2024-09-19 11:09:13 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'; // Import Tailwind CSS
-import App from './App.jsx';
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'; // Import Provider dari React-Redux
+import { store } from './app/store'; // Import store Redux
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Bungkus App dengan Provider untuk memberikan akses store Redux */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
